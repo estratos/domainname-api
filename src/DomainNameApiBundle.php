974a0d31d398787124f4360c6e5287a19c275b46
Julio Rodriguez <julio@estratos.net>
2025-10-30 14:45:15 -0600

    rewrite code structure

---
+++ src/DomainNameApiBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Estratos\DomainNameApi;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class DomainNameApiBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
+
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+}
