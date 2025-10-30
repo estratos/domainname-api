826b9fd75e4cf2e3ec3434c9784675dc72b5a2a0
Julio Rodriguez <julio@estratos.net>
2025-10-30 16:27:49 -0600

    fix error

--- src/DomainNameApiBundle.php
+++ src/DomainNameApiBundle.php
@@ -2,33 +2,22 @@
 
 namespace Estratos\DomainNameApi;
 
-use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class DomainNameApiBundle extends AbstractBundle
 {
-    public function configure(DefinitionConfigurator $definition): void
-    {
-        $definition->import('../config/definition.php');
-    }
-
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         // Cargar servicios
         $container->import('../config/services.yaml');
 
-        // Configurar el servicio principal con los parámetros
-        $container->services()
-            ->get(Estratos\DomainNameApi\Service\DomainNameApiClient::class)
-            ->arg('$username', $config['username'])
-            ->arg('$password', $config['password'])
-            ->arg('$testMode', $config['test_mode'])
-            ->arg('$config', [
-                'timeout' => $config['timeout'],
-                'default_nameservers' => $config['default_nameservers'],
-            ]);
+        // Configurar parámetros
+        $builder->setParameter('domainname_api.username', $config['username'] ?? '');
+        $builder->setParameter('domainname_api.password', $config['password'] ?? '');
+        $builder->setParameter('domainname_api.test_mode', $config['test_mode'] ?? false);
+        $builder->setParameter('domainname_api.timeout', $config['timeout'] ?? 30);
     }
 
     public function getPath(): string
